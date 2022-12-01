1919fd9afdd78e5d10895a6f00234a315e45603e
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2022-12-01 13:23:24 +0100

    Project setup and day 1 solutions

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+    application
+}
+
+group = "de.lennartegb"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
