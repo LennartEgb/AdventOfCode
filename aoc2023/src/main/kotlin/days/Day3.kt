d9ecb78b415254e06cd130578674ccc217c8d598
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2023-12-11 17:23:30 +0000

    Just use the solution by ephemist as I don't get why my data structure is not working :crying:

--- aoc2023/src/main/kotlin/days/Day3.kt
+++ aoc2023/src/main/kotlin/days/Day3.kt
@@ -2,71 +2,36 @@ package days
 
 import Day
 
-private typealias SchematicRow = List<Day3.Element>
+private data class IntPair(val first: Int, val second: Int) {
+    override fun toString(): String = "($first, $second)"
+}
+
+private infix fun Int.to(other: Int): IntPair = IntPair(this, other)
 
 // It's not: 542418
 object Day3 : Day {
+    private val NUMBER = """\d+""".toRegex()
+    private fun Char.isSymbol(): Boolean = this != '.' && !isWhitespace() && !isDigit()
 
-    sealed interface Element
-    data class Symbol(val value: Char, val column: Int, val row: Int) : Element
-    data class Number(val value: Int, val xRange: IntRange, val row: Int) : Element {
-        val expandedColumn: IntRange = xRange.first - 1..xRange.last + 1
-        val expandedRow: IntRange = row - 1..row + 1
-    }
-
-    private val numberRegex = """\d+""".toRegex()
-    private val symbolRegex = """[^.;^\d^\n]+""".toRegex()
-
-    override fun part1(input: List<String>): Any {
-        val elements = input.mapIndexed(::extractElements).flatten()
-
-        println("----------- manual check -----------")
-        println("Found numbers: ${elements.filterIsInstance<Number>().size}")
-        println("Found symbols: ${elements.filterIsInstance<Symbol>().size}")
-
-        println("----------- Regex check -----------")
-        println("Found numbers: ${numberRegex.findAll(input.joinToString("\n")).count()}")
-        println("Found numbers: ${symbolRegex.findAll(input.joinToString("\n")).count()}")
-
-        return elements.findParts().sumOf { it.value }
-    }
-
-    override fun part2(input: List<String>): Any {
-        return "Implement"
-    }
-
-    private fun extractElements(row: Int, line: String): List<Element> = buildList {
-        var numberStart = -1
-        var numberValue = ""
-        for ((column, c) in line.withIndex()) {
-            when {
-                c.isDigit() -> {
-                    numberValue += c
-                    if (numberStart == -1) numberStart = column
-                }
-
-                else -> {
-                    if (c != '.') add(Symbol(value = c, column = column, row = row))
-                    if (numberValue.isNotBlank()) {
-                        add(Number(value = numberValue.toInt(), xRange = numberStart..column, row = row))
-                        numberStart = -1
-                        numberValue = ""
+    private fun parts(input: List<String>): Map<IntPair, List<Int>> = mutableMapOf<IntPair, MutableList<Int>>().apply {
+        for ((y, line) in input.withIndex()) {
+            for (match in NUMBER.findAll(line)) {
+                val number = match.value.toInt()
+                for (y2 in (y - 1).coerceAtLeast(0)..(y + 1).coerceAtMost(input.lastIndex)) {
+                    val line2 = input[y2]
+                    val x0 = match.range.first - 1
+                    val x1 = match.range.last + 1
+                    for (x in x0.coerceAtLeast(0)..x1.coerceAtMost(line2.lastIndex)) {
+                        if (line2[x].isSymbol()) {
+                            getOrPut(x to y2) { mutableListOf() } += number
+                        }
                     }
                 }
             }
         }
-        if (numberValue.isNotBlank()) {
-            add(Number(value = numberValue.toInt(), xRange = numberStart..line.length, row = row))
-        }
     }
 
-    private fun SchematicRow.findParts(): List<Number> {
-        val numbers = filterIsInstance<Number>()
-        val symbols = filterIsInstance<Symbol>()
-        return numbers.filter { number -> symbols.any { symbol -> symbol in number } }
-    }
-
-    private operator fun Number.contains(symbol: Symbol): Boolean =
-        symbol.row in expandedRow && symbol.column in expandedColumn
+    override fun part1(input: List<String>): Any = parts(input).values.sumOf { it.sum() }
+    override fun part2(input: List<String>): Any = parts(input).values.sumOf { if (it.size == 2) it[0] * it[1] else 0 }
 }
 
